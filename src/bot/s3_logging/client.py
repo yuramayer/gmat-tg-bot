b1a08eea90642decf597c8079e24f792fd459994
yuramayer <votetopovorot2015@gmail.com>
2025-09-07 11:14:42 +0300

    feat: created s3 classes for the logging

--- src/bot/s3_logging/client.py
+++ src/bot/s3_logging/client.py
@@ -1,1 +1,55 @@
-"""S3 Client (cloud) для логирования"""
+"""S3 Client wrapper for the cloud logging"""
+
+import json
+import boto3
+from botocore.config import Config
+
+
+class S3Client:
+    """The wrapper around boto3 S3 client"""
+
+    def __init__(
+            self,
+            access_key: str,
+            secret_key: str,
+            bucket_name: str,
+            endpoint_url: str = "https://storage.yandexcloud.net",
+            region_name: str = "ru-central1"
+            ):
+        """Initialize the S3 Client"""
+        self.access_key = access_key
+        self.secret_key = secret_key
+        self.bucket_name = bucket_name
+        self._s3 = boto3.client(
+            service_name="s3",
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            endpoint_url=endpoint_url,
+            region_name=region_name,
+            config=Config(signature_version="s3v4")
+        )
+    
+    @staticmethod
+    def get_log_key(
+        timestamp: str,
+        user_id: int
+        ) -> str:
+        """Generate partitioned S3 key for the log"""
+        date, time_str = timestamp.split('T')
+        hour = time_str[:2]
+        return f"date={date}/hour={hour}/{user_id}_{timestamp}.json"
+    
+    def send_logs(
+            self,
+            logs_json: dict,
+            key: str
+            ) -> None:
+        """Upload log dict as JSON to S3"""
+        self._s3.put_object(
+            Bucket=self.bucket_name,
+            Key=key,
+            Body=json.dumps(
+                logs_json,
+                ensure_ascii=False
+            ).encode("utf-8")
+        )
--- src/bot/s3_logging/s3_logger.py
+++ src/bot/s3_logging/s3_logger.py
@@ -1,1 +1,82 @@
-"""S3 Logger объект"""
+"""Wrapper for structured logging into S3"""
+
+from datetime import datetime, timezone
+from dataclasses import dataclass, field, asdict
+from aiogram.types import Message
+from src.bot.s3_logging.client import S3Client
+
+
+@dataclass
+class MessageLog:
+    """
+    Structured data object
+    representing bot interaction event
+    """
+    timestamp: str = field(
+        default_factory=lambda: datetime.now(
+            timezone.utc
+        ).isoformat()
+    )
+    chat_id: int = 0
+    message_id: int = 0
+    direction: str = ""
+    text: str = ""
+    router: str = ""
+    method: str = ""
+    event_type: str = ""
+
+
+class S3Logger:
+    """
+    High-level logger that serialized & uploads
+    bot events into S3
+    """
+    def __init__(self, s3_client: S3Client):
+        """
+        Args:
+            s3_client: initialized S3Client instance
+        """
+        self._s3_client = s3_client
+
+    def log_message(
+            self,
+            *,
+            message: Message,
+            direction: str,
+            text: str,
+            router: str,
+            method: str,
+            event_type: str
+        ) -> None:
+        """
+        Log the action & send the log to the S3
+
+        Args:
+            message (aiogram.types.Message): TG
+                message that connected with log
+            direction (str): type of the message,
+                should be inbound or outbound
+            text (str): text for the log
+            router (str): name of the router
+                where the event happened
+            method (str): name of the func
+                where the event happened
+            event_type (str): type of the action
+        """
+        log_obj = MessageLog(
+            chat_id=message.chat.id,
+            message_id = message.message_id,
+            direction=direction,
+            text=text,
+            router=router,
+            method=method,
+            event_type=event_type
+        )
+        log_dict = asdict(log_obj)
+        log_key = self._s3_client.get_log_key(
+            datetime.now().isoformat(),
+            message.chat.id
+        )
+        self._s3_client.send_logs(
+            log_dict, log_key
+        )
