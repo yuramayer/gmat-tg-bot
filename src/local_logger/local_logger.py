22e3595000222c8bb28467f8b2c2e4c88fd714f0
yuramayer <votetopovorot2015@gmail.com>
2025-09-07 09:48:30 +0300

    docs: added docstring for the local logger

--- src/local_logger/local_logger.py
+++ src/local_logger/local_logger.py
@@ -1,3 +1,5 @@
+"""Local logger class"""
+
 import logging
 
 
@@ -9,7 +11,14 @@ class LocalLogger:
             name: str = "local_logger",
             level: int = logging.INFO
             ):
-        """Initialize logger object"""
+        """
+        Initialize logger object
+        
+        Args:
+            name (str, optional): name for the current logger,
+                default = 'local_logger'
+            level (int, optional): logging level, default = 'INFO'
+        """
         self.logger = logging.getLogger(name)
         self.logger.setLevel(level)
 
@@ -24,16 +33,46 @@ class LocalLogger:
             self.logger.addHandler(handler)
     
     def debug(self, msg: str, *args, **kwargs) -> None:
+        """
+        Debug logger method
+
+        Args:
+            msg (str): message to log
+        """
         self.logger.debug(msg, *args, **kwargs)
     
     def info(self, msg: str, *args, **kwargs) -> None:
+        """
+        Info logger method
+
+        Args:
+            msg (str): message to log
+        """
         self.logger.info(msg, *args, **kwargs)
 
     def warning(self, msg: str, *args, **kwargs) -> None:
+        """
+        Warning logger method
+
+        Args:
+            msg (str): message to log
+        """
         self.logger.warning(msg, *args, **kwargs)
     
     def error(self, msg: str, *args, **kwargs) -> None:
+        """
+        'Error message' logger method
+
+        Args:
+            msg (str): message to log
+        """
         self.logger.error(msg, *args, **kwargs)
 
     def critical(self, msg: str, *args, **kwargs) -> None:
+        """
+        'Critical message' logger method
+
+        Args:
+            msg (str): message to log
+        """
         self.logger.critical(msg, *args, **kwargs)
